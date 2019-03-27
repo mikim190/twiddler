bfbfb66a0e28292a98561c04b1431ebe13f83298
Kim T <kim200@kims-mbp.attlocal.net>
2019-03-26 22:19:17 -0700

    Complete basic requirements

--- app.js
+++ app.js
@@ -1,65 +1,72 @@
 $(document).ready(function(){ //have the JQuery data ready
-  var $body = $('.tweets'); //defines JQ '.tweets' element as variable $body
+  var $body = $('body'); //defines JQ '.tweets' element as variable $body
   // $body.html(''); //replace all contents in HTML body with empty string
-  var index = streams.home.length - 1; //start from index -1
+  var index = streams.home.length - 1; //access to global data, start from tweet at index 10
+  var latest = 0; //keep track number of tweets displayed in DOM
   while(index >= 0){ 
     var tweet = streams.home[index];//this equals to object tweet {name:"",message:"",date:00}
-    var $tweet = $("<div class='tweet'></div>");//creates a new <div>, only exists in the local memory,= var 
-    var $user = $("<p class='user'></p>");
-    var $message = $("<p class='message'></p>");
-    var $time = $("<span class='time'></span>");
-    // $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ' + tweet.created_at); //text() method: set content to each <$tweet> element ->creates a string "@user:message, date/time"
-    $tweet.appendTo(".main"); //inserts content from $tweet to $body
-    //add style to username(in bold), then set content for it, then inserts into $tweet
-    $user.addClass(".user").text("@" + tweet.user+ ": ").appendTo($tweet); //"@username:"
-    $message.addClass(".message").text(" " + tweet.message).appendTo($tweet); 
-    $time.text(" " + tweet.created_at).appendTo($tweet);
-    index -= 1; //decrements the index after inserting tweet into $body
+    var $main = $('<section class="main"></section>');
+    $main.appendTo($body)
+    var $tweet = $('<div class="tweet"></div>'); //creates a new <div>, only exists in the local memory,= var 
+    $tweet.text(' ' + tweet.message + ' '); //text() method: set content to each <$tweet> element 
+    $tweet.appendTo($main); //inserts content from $tweet to $main section
+    var $timestamp = $('<div class="timestamp"></div>');
+    $timestamp.text(' ' + tweet.created_at + ' '); 
+    $timestamp.appendTo($main);
+    var $user = $('<a class="user" data-value="' + tweet.user + '"></a>');
+    $user.addClass(".user").text('@' + tweet.user + ': '); //add style to username(in bold), then set content for it
+    $user.prependTo($main); //inserts username in front of other content
+    index --; //decrease the index after inserting tweet into $body
+    latest++; //increase number of tweets after adding into DOM
   };
-// });
 
-  // access data from window 
-  // var bigData = window.streams.home //[{},{},{}]
+  //click button to get new Tweets //similar to above method
 
-    //button to get new Tweets  
-  $("button").on("click", function (){
-    generateRandomTweet();
-    var $newUser = $("<p class='user'></p>");
-    var $newMessage = $("<p class='message'></p>");
-    var $newTime = $("<span class='time'></span>");
-    var data = streams.home.pop(); //get data from window
-    // var data = bigData.slice(i, i+1); 
+  $("button").on("click", function (){  
+    index = streams.home.length - 1; //access to global data, start from bottom
+    while(index >= latest) { // index decreases, latest increases after adding each tweet into DOM 
+      var newTweet = streams.home[latest];
+      var $main = $('<section class="main"></div>');
+      $main.appendTo($body)
+      var $tweet = $('<div class="tweet"></div>');
+      $tweet.text(' ' + newTweet.message + ' ');
+      $tweet.appendTo($main);
+      var $timestamp = $('<div class="timestamp"></div>')
+      $timestamp.text(' ' + newTweet.created_at);
+      $timestamp.appendTo($main);
+      var $user = $('<a class="user" data-value="' + newTweet.user + '"></a>');
+      $user.text('@' + newTweet.user + ': ');
+      $user.prependTo($main);
+      latest++;
+      };
+  });  
 
-    var $newTweet = $("<div class='newTweet'></div>"); //initialize new <div> in DOM, = var
+  //Show username's timeline
+  //watch document, triggers click event on <a>/anchor selector, invoke fn:
+  $(document).on("click", "a", function() {
+    var username = $(this).data("value"); //"this" refers to <a> selector, data(key): get data named "value"
+    $body.html(''); //html('')is used to set an element's content, any content that was in that element is completely replaced by the new content.
+    var $twiddUser = $('<header><h1>Twiddler - ' + username +'</h1></header>');
+    $twiddUser.appendTo($body);
+    var $home = $('<div class="home"><a href="index.html">Main Page</a></div>');
+    $home.appendTo($body);
+    var userIndex = streams.users[username].length - 1;
+    while (userIndex >= 0) {
+      var userTweet = streams.users[username][userIndex];
+      var $main = $('<section class="main"></section>');
+      $main.appendTo($body)
+      var $tweet = $('<div class="tweet"></div>');
+      $tweet.text(userTweet.message);
+      $tweet.appendTo($main);
+      var $timestamp = $('<div class="timestamp"></div>');
+      $timestamp.text(userTweet.created_at);
+      $timestamp.appendTo($main);
+      var $user = $('<a class="user" data-value="' + userTweet.user + '"></a>');
+      $user.text('@' + userTweet.user + ': ');
+      $user.prependTo($main);
+      userIndex -= 1;
+      }
+    });
 
-    $newTweet.appendTo(".newfeed"); //inserts newTweet "@..." into <div tweets>
-    $newUser.addClass(".user").text("@" + data.user + ": ").appendTo($newTweet);
-    $newMessage.addClass(".message").text(" " + data.message).appendTo($newTweet);
-    $newTime.text(" " + data.created_at).appendTo($newTweet);
-      // $newTweet.text($newUser + $newMessage + $newTime)
-      // $newTweet.addClass(".newTweet").text("@" + data.user + ': ' + data.message + ' ' + data.created_at); //set content to each $newTweet elemnt      
-  });
-  
-
-  //when click on name of user ->retrieve name and display in body as username's timeline
-  $('.tweets').on('click', '.tweet .user', function(event){ //event = "click"
-        // event "click" selected element ".user" will not be triggered
-          event.preventDefault();
-          // alert(event);
-          //text(): Returns value from ".user"
-          var user = $(this).text(); 
-          //remove everything from timeline
-          //initialize user's name timeline and inserts into $body
-          var $timeline = $(`<h1 class=timeline> ${user}'s Timeline</h1>`);
-          $timeline.prependTo($body);
-          $('.new').hide(); 
-          //hide all tweet that not contain our user's name
-          $('.tweet').not("div:contains('" + user + "')").hide(400);
-          //show all tweet that contains our user's name
-          $("div:contains('" + user + "')").show(400);
-          
-        });
-
-
-});
+}); //main closing bracket
     
